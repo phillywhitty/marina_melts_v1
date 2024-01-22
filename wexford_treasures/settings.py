a4405c53fd7e5fe4ea577d97b9bcf9f995b4d926
phillywhitty <philipwhitty92@yahoo.ie>
2024-01-22 17:58:11 +0000

    remove / in heroku url

--- wexford_treasures/settings.py
+++ wexford_treasures/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['wexford-treasures-afe33013c7ab.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['wexford-treasures-afe33013c7ab.herokuapp.com', 'localhost']
 
 
 # Application definition
