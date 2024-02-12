eb96c3e9b17fbf8ee1a7492a90427d294859b307
phillywhitty <philipwhitty92@yahoo.ie>
2024-02-12 10:55:24 +0000

    add wishitem and my wallet to admin in profiles

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
-from .models import MyWallet
+from .models import MyWallet, WishItem
 from products.models import Product
-from profiles.models import WishItem
+
 
 
 class MyWalletAdmin(admin.ModelAdmin):
     list_display = ('user', 'card_number', 'expire_number', 'cvv_number')
 
 
-class WishItem(admin.ModelAdmin):
+class WishItemAdmin(admin.ModelAdmin):
     list_display = ('user', 'product', 'quantity')
+
+
+admin.site.register(MyWallet, MyWalletAdmin)
+admin.site.register(WishItem, WishItemAdmin)
